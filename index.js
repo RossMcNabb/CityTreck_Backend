f1c85fa39d6b83878195ee9bc6d1a5236406375b
Ross McNabb <rossmcnabb@ymail.com>
2022-08-22 13:03:09 +0100

    app.get multiselect 1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const cors = require("cors");
 const app = express();
+const mysql = require("mysql");
 
-const db = require("mysql-promise")();
-
-db.configure({
+const db = mysql.createPool({
   host: "database-1.crurl47d1sgo.eu-west-2.rds.amazonaws.com",
   user: "admin",
   password: "Password1",
@@ -13,6 +13,10 @@ db.configure({
   database: "final_project",
 });
 
+db.getConnection(function (err) {
+  if (err) throw err;
+  console.log("connection successful");
+});
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
@@ -20,8 +24,9 @@ app.get("/", (req, res) => {
   res.send("Hell0");
 });
 
-app.get("/pathway", async (req, res) => {
-  var data = [];
+app.get("/pathway", (req, res) => {
+
+  var data =[];
 
   const city = req.query.city;
   const restaurantType = req.query.restaurantType;
@@ -29,30 +34,29 @@ app.get("/pathway", async (req, res) => {
   const mobility = req.query.mobility;
   const cuisine = req.query.cuisine;
 
-  console.log(req.query.restaurantType);
+  console.log(req.query.restaurantType[0])
 
-  const sqlSelectEating =
-    "SELECT * FROM eating_and_drinking WHERE city=? AND restaurant_type=? AND mobility_level=? and food_category=?";
-  const sqlSelectAttractions =
-    "SELECT * FROM attractions WHERE city=? AND attraction_type=? AND mobility_level=?";
+  const sqlSelectEating ="SELECT * FROM eating_and_drinking WHERE city=? AND mobility_level=? and food_category=? AND restaurant_type=? or ? or ?";
+  const sqlSelectAttractions = "SELECT * FROM attractions WHERE city=? AND attraction_type=? AND mobility_level=?";
+   
+    db.query(sqlSelectEating, [city, mobility,cuisine, restaurantType[0],restaurantType[1],restaurantType[2]], (err, results1) => {
+      if (err) {
+        throw err;
+      }
+  
+      data.push(results1)
+    });
+    db.query(sqlSelectAttractions, [city, attractionType, mobility], (err, results2) => {
+    if (err) {
+      throw err;
+    }
 
-  const [results1] = await db.query(sqlSelectEating, [
-    city,
-    restaurantType,
-    mobility,
-    cuisine,
-  ]);
-  const [results2] = await db.query(sqlSelectAttractions, [
-    city,
-    attractionType,
-    mobility,
-  ]);
-  const results = [...results1, ...results2];
-  return res.status(200).json(results);
+ data.push(results2)
+console.log(data)
+res.send(data)
+});
 });
 
 app.listen(3001, () => {
   console.log("running on port 3001");
 });
-
-module.exports = app;
